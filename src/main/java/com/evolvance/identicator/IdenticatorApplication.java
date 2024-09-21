e963b99404dfdc4d9a4b765371f031bb4f4721d6
Luv Dhamija <luv.dhamija.official@gmail.com>
2024-09-21 13:13:33 +0530

    Initial Setup (#1)
    * Initial Setup of the project
    ---------
    
    Co-authored-by: Luv Dhamija <luv.dhamija.official@gmail.com>

---
+++ src/main/java/com/evolvance/identicator/IdenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.evolvance.identicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evolvance/identicator/IdenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolvance.identicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
